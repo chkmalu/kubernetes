176137ca32379971facbc3eed5ae2e4994b54ea8
malu <chkmalu@gmail.com>
2023-02-01 21:15:21 +0100

    helmfile

--- microservice/install-charts.sh
+++ microservice/install-charts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+#old fashion way of deploying helm chart
 helm install adservice charts/microservices/ -f values/adservice-values.yaml
 helm install cartservice charts/microservices/ -f values/cartservice-values.yaml
 helm install checkoutservice charts/microservices/ -f values/checkoutservice-values.yaml
 helm install currencyservice charts/microservices/ -f values/currencyservice-values.yaml
 helm install emailservice charts/microservices/ -f values/emailservice-values.yaml
-helm install frontend charts/microservices/ -f values/frontend-values.yaml
+helm install frontend charts/frontend -f values/frontend-values.yaml
 helm install paymentservice charts/microservices/ -f values/paymentservice-values.yaml
 helm install productcatalogservice charts/microservices/ -f values/productcatalogservice-values.yaml
 helm install recommendationservice charts/microservices/ -f values/recommendationservice-values.yaml
--- microservice/uninstall-chart.sh
+++ microservice/uninstall-chart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-helm uninstall adservice charts/microservices/ -f values/adservice-values.yaml
-helm uninstall cartservice charts/microservices/ -f values/cartservice-values.yaml
-helm uninstall checkoutservice charts/microservices/ -f values/checkoutservice-values.yaml
-helm uninstall currencyservice charts/microservices/ -f values/currencyservice-values.yaml
-helm uninstall emailservice charts/microservices/ -f values/emailservice-values.yaml
-helm uninstall frontend charts/microservices/ -f values/frontend-values.yaml
-helm uninstall paymentservice charts/microservices/ -f values/paymentservice-values.yaml
-helm uninstall productcatalogservice charts/microservices/ -f values/productcatalogservice-values.yaml
-helm uninstall recommendationservice charts/microservices/ -f values/recommendationservice-values.yaml
-helm uninstall redis-cart charts/redischart/ -f values/redis-cart-values.yaml
-helm uninstall shippingservice charts/microservices/ -f values/shippingservice-values.yaml
+#old fashion way
+helm uninstall adservice
+helm uninstall cartservice
+helm uninstall checkoutservice
+helm uninstall currencyservice
+helm uninstall emailservice
+helm uninstall frontend
+helm uninstall paymentservice
+helm uninstall productcatalogservice
+helm uninstall recommendationservice
+helm uninstall redis-cart
+helm uninstall shippingservice
